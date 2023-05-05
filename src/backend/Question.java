961e2b0e4396a81800dfbdc1687463dda79c73e9
Newbuntu <giovannistagnaro20@gmail.com>
2023-05-04 18:48:31 -0700

    Created Questino class

---
+++ src/backend/Question.java
@@ -0,0 +1,30 @@
+package backend;
+
+public class Question {
+
+    private String question;    // question from user
+
+    /*
+     * Takes in a question formatted as a String
+     */
+    public Question(String question) {
+
+        // makes sure that the question is not null
+        if (question == null) {
+            throw new IllegalArgumentException("Entered question was null");
+        }
+        else {
+            this.question = question;
+        }
+    }
+
+    /*
+     * Returns the question that was asked.
+     * 
+     * @return String: user question
+     */
+    public String getQuestion() {
+        return this.question;
+    }
+
+}
