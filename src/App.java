621460b06e0e728431d0687dc00f2a158f5a237c
brandoluu <brandonluu9@gmail.com>
2023-05-04 21:24:13 -0700

    adding workflow and adding custom JButton for the newQuestion

--- src/App.java
+++ src/App.java
@@ -1,5 +1,30 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.swing.*;
+
+import frontend.questionButton;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        new questionButton();
     }
+}
+
+
+class AppFrame extends JFrame {
+
 }
