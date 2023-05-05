e2c96592f6b0f3a534de8d7e37cac76e54199827
hlm01 <35647316+hlm01@users.noreply.github.com>
2023-05-05 01:02:58 -0700

    integrated question button

--- src/App.java
+++ src/App.java
@@ -20,7 +20,7 @@ class AppFrame extends JFrame {
 
         //Implement OpenSans ttf file to panel font
         File fontFile = new File("fonts/OpenSans-Regular.ttf");
-        Font font = null;
+        Font font;
         try {
             font = Font.createFont(Font.TRUETYPE_FONT, fontFile);
         } catch (FontFormatException | IOException e) {
@@ -42,6 +42,7 @@ class AppFrame extends JFrame {
             historyPanel.addHistoryButton(h);
         }
         revalidate();
+        this.add(new questionButton(), BorderLayout.SOUTH);
 
     }
 }
@@ -49,6 +50,5 @@ class AppFrame extends JFrame {
 public class App {
     public static void main(String[] args) {
         new AppFrame();
-        new questionButton();
     }
 }
