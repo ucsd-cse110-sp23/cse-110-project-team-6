a53b543c2d5a21a3299d0d27dc547c61a4c417d0
Newbuntu <giovannistagnaro20@gmail.com>
2023-05-08 13:54:05 -0700

    updated the history file path in AppPanels

--- src/frontend/AppPanels.java
+++ src/frontend/AppPanels.java
@@ -12,6 +12,6 @@ public class AppPanels extends JPanel{
     protected final static Color GREY = new Color(52, 53, 62);
     protected final static Color LIGHT_GREY = new Color(100,100,100);
     protected MyFont myFont;
-    protected final String historyFilePath = "src/backend/history.txt";
+    protected final String historyFilePath = "src/backend/history.json";
     
 }
