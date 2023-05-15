ac9dee2fd8282ba14bb408484e439adb62371190
hlm01 <35647316+hlm01@users.noreply.github.com>
2023-05-14 23:45:27 -0700

    added story test 5

---
+++ tests/StoryTest/StoryTest5.java
@@ -0,0 +1,24 @@
+package StoryTest;
+
+import middleware.HistoryManager;
+import middleware.SayItAssistant;
+import middleware.WhisperRequest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class StoryTest5 {
+    @Test
+    public void testClearAllEmpty() {
+       /* Scenario 2: There are no questions in SayIt Assistant’s history
+        Given: there are no questions SayIt Assistant’s history
+        When: History Helen wants to click the clear all button
+        Then: Nothing will happen.*/
+        HistoryManager newHistoryManager = new HistoryManager(new SayItAssistant(new WhisperRequest()));
+        Assertions.assertEquals(newHistoryManager.getQuestions(), new ArrayList<>());
+        newHistoryManager.clearAll();
+        Assertions.assertEquals(newHistoryManager.getQuestions(), new ArrayList<>());
+        Assertions.assertEquals(newHistoryManager.getHistorySize(), 0);
+    }
+}
