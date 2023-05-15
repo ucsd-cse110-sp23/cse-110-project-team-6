28d996d86e9dd9377f5fe5beb4f358d5d7d765af
Newbuntu <giovannistagnaro20@gmail.com>
2023-05-15 00:20:53 -0700

    Story Test 3

--- tests/StoryTest/ThirdStoryTest.java
+++ tests/StoryTest/ThirdStoryTest.java
@@ -1,116 +1,54 @@
 package StoryTest;
 
-import javax.sound.sampled.*; // For sound recording and playback
 import org.junit.jupiter.api.*;
-
-import frontend.HistoryPanel;
-import frontend.NewQuestionPanel;
-import frontend.QnAPanel;
-import middleware.Answer;
-import middleware.HistoryManager;
-import middleware.IAPIRequest;
-import middleware.MockTargetDataLine;
-import middleware.Question;
-import middleware.SayItAssistant;
-import middleware.VoiceRecorder;
-
 import static org.junit.jupiter.api.Assertions.*;
 
+import middleware.*;
+
 import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.util.ArrayList;
 
-public class StoryTestThree {
-
-     class MockWhisperAPIQ1 implements IAPIRequest{
-        File file; 
-    
-        MockWhisperAPIQ1(File filePath){
-            this.file = filePath;
-        }
-    
-        @Override
-        public String callAPI() {
-            return "Summarize the history of inequality of the visually impaired in relation to literacy before Braille";
-        }
-        
-    }
-    
-    class MockChatGPTAPIQ1 implements IAPIRequest{
-        String prompt; 
-    
-        MockChatGPTAPIQ1(String prompt){
-            this.prompt = prompt; 
-        }
-        @Override
-        public String callAPI() {
-            return "There are many things to list";
-        }
-        
-    }
-
-    MockTargetDataLine mockMic;
-    VoiceRecorder newAudio; 
-    HistoryManager previousQuestions;
-    Path path; 
-
-    @BeforeEach
-    public void initialize(){
-        mockMic = new MockTargetDataLine();
-        newAudio = new VoiceRecorder(mockMic);
-    }
-
-    /*
-     * Scenario 1: Voice prompt button is pressed to ask a new question
+/**
+ * Story test for verficiation that vocalized questions are recorded.
+ * 
+ * For sake of testing, UI elements will not be factored in. Any UI elements which may have been
+ * specified will be ommitted from description of the tests as (...) or brief context description.
+ */
+public class ThirdStoryTest {
+    /**
+     * New Question button is pressed to ask a new question
+     * 
      * Given: History Helen has pressed on the new question button
      * When: History Helen holds the voice prompt button 
      * And: Helen starts asking the SayIt Assistant the prompt “Summarize the history of inequality of the visually impaired…”
      * Then: SayIt Assistant’s voice prompt button changes color 
      * And: Helen’s speech is recorded
      * When: History Helen continues to expand on her question by asking “...in relation to literacy before Braille.”
-     * Then: Helen’s speech will still be recorded
+     * Then: Helen’s speech will still be recordedGiven: History Helen has SayIt Assitant opened
+     * And: Helen has not asked any questions
+     * When: History Helen looks at the history
+     * Then: History Helen sees that there are no questions asked
      */
     @Test
-    public void TestBDD1() {
-        AudioFormat format = newAudio.getAudioFormat();
-        assertNotNull(format);
-        assertEquals(44100, format.getSampleRate(), 0.1);
-        assertEquals(16, format.getSampleSizeInBits());
-        assertEquals(1, format.getChannels());
-        assertEquals(true, format.isBigEndian());
-
-        newAudio.startRecording();
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        
-        newAudio.stopRecording();
-
-        path = Path.of("prompt.wav");
-        assertTrue(Files.exists(path));
-
-        File mockFile = new File("prompt.wav");
-        MockWhisperAPIQ1 mockWhipser = new MockWhisperAPIQ1(mockFile);
-        assertEquals("Summarize the history of inequality of the visually impaired in relation to literacy before Braille", mockWhipser.callAPI());
-
-        SayItAssistant sayItAssistant = new SayItAssistant(mockWhipser);
-        previousQuestions = new HistoryManager(sayItAssistant);
-        NewQuestionPanel newQuestionPanel = new NewQuestionPanel(sayItAssistant, new QnAPanel(), new HistoryPanel(previousQuestions));
-
-        
+    public void testScenario1() {
+        /*
+         * This is tested manually, as everything in this scenario is based around the UI
+         */
     }
-    
+
     /*
+     * New question button is depressed to stop the question
+     * 
      * Given: History Helen has finished asking a question about “Summarizing the history of inequality of the visually impaired in relation to literacy before Braille” 
      * When: History Helen releases the voice prompt button
-     * Then: SayIt Assistant transcribes her question.
+     * Then: SayIt Assistant transcribes her question (User Story 3)
      * And: SayIt Assistant displays the answer to her question.
      */
     @Test
-    public void TestBDD2() {
-
+    public void TestScenario2() {
+        /*
+         * This is also tested manually, as everything in this scenario is also based around the UI.
+         */
     }
-
+    
 }
