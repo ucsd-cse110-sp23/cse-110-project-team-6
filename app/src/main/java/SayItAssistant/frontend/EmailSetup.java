d8ad60b135f50a76309b0842b7d776f27cd29858
林耕廉 <keng@hayashikourens-MacBook-Air.local>
2023-06-04 11:48:24 -0700

    Added get method to make previous info avaliable
    added python server and fixed tests
    
    Added US11 tests, modified MS1-US1 tests, modified PromptFactory to strip command from prompt

--- app/src/main/java/SayItAssistant/frontend/EmailSetup.java
+++ app/src/main/java/SayItAssistant/frontend/EmailSetup.java
@@ -1,11 +1,14 @@
 package SayItAssistant.frontend;
 
+import java.awt.GridLayout;
+import java.awt.Image;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -29,23 +32,52 @@ public class EmailSetup{
         JTextField tls = new JTextField(columns);
         JTextField password = new JPasswordField(columns);
 
-        JPanel panel = new JPanel();
+        
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(String.format("https://hlnm.pythonanywhere.com/emails?user=%s&pass=%s", username, pwd)))
+            .build();
+
+        client.sendAsync(request, BodyHandlers.ofString())
+            .thenApply(HttpResponse::body)
+            .thenAccept(responseBody -> {
+                // Parse the JSON response
+                JSONObject jsonResponse = new JSONObject(responseBody);
+
+                // Access the "userinfo" object within the JSON response
+                //JSONObject userInfo = jsonResponse.getJSONObject("userinfo");
+                last.setText(jsonResponse.getString("last_name"));
+                first.setText(jsonResponse.getString("first_name"));
+                display.setText(jsonResponse.getString("display_name"));
+                email.setText(jsonResponse.getString("email_address"));
+                smtp.setText(jsonResponse.getString("smtp_host"));
+                tls.setText(jsonResponse.getString("tls_port"));
+                password.setText(jsonResponse.getString("email_password"));
+            })
+            .join();
+            
+        
+        JPanel panel = new JPanel(new GridLayout(7,1,10,10));
         panel.add(new JLabel("First Name"));
         panel.add(first);
         panel.add(new JLabel("Last Name"));
         panel.add(last);
-        panel.add(new JLabel("display name"));
+        panel.add(new JLabel("Display Name"));
         panel.add(display);
-        panel.add(new JLabel("email address"));
+        panel.add(new JLabel("Email Address"));
         panel.add(email);
-        panel.add(new JLabel("SMTP host"));
+        panel.add(new JLabel("SMTP Host"));
         panel.add(smtp);
         panel.add(new JLabel("TLS Port"));
         panel.add(tls);
-        panel.add(new JLabel("email password"));
+        panel.add(new JLabel("Email Password"));
         panel.add(password);       
-        Icon i = new ImageIcon("emailicon.png");
-        if(JOptionPane.showConfirmDialog(null,panel, "Setup Email", JOptionPane.OK_CANCEL_OPTION,1, i) == JOptionPane.OK_OPTION){
+        ImageIcon imageIcon = new ImageIcon("emailicon.png");
+        Image image = imageIcon.getImage();
+        Image newimg = image.getScaledInstance(120, 120, java.awt.Image.SCALE_SMOOTH);
+        imageIcon = new ImageIcon(newimg);
+        
+        if(JOptionPane.showConfirmDialog(null,panel, "Setup Email", JOptionPane.OK_CANCEL_OPTION,1, imageIcon) == JOptionPane.OK_OPTION){
             // Create the JSON object
             JSONObject jsonObject = new JSONObject();
             // Get the text from each JTextField and add it to the JSON object
@@ -75,13 +107,13 @@ public class EmailSetup{
 
             // Print the JSON string
             System.out.println(jsonString);
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
+            //HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request1 = HttpRequest.newBuilder()
                 .uri(URI.create(String.format("https://hlnm.pythonanywhere.com/emails?user=%s&pass=%s",username,pwd)))
                 .header("Content-Type", "application/json")
                 .PUT(BodyPublishers.ofString(jsonString))
                 .build();
-            client.sendAsync(request, BodyHandlers.ofString())
+            client.sendAsync(request1, BodyHandlers.ofString())
                 .thenApply(HttpResponse::body)
                 .thenAccept(System.out::println)
                 .join();
---
+++ app/src/test/java/SayItAssistant/MilestoneTwoStoryTest/EleventhStoryTest.java
@@ -0,0 +1,55 @@
+package SayItAssistant.MilestoneTwoStoryTest;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import SayItAssistant.frontend.PromptPanel;
+import SayItAssistant.middleware.IPrompt;
+import SayItAssistant.middleware.PromptFactory;
+import SayItAssistant.middleware.Question;
+
+/*
+ * Class containing all of the tests for the new question command
+ */
+public class EleventhStoryTest {
+   
+    private static final String PROMPT1 = "Question. What caused the Roman Empire to fall?";
+    private static final String INVALID_QUESTION_COMMAND = "Question.";
+
+    /*  BDD Scenario Test for Scenario 1: Prompt starts with the word “Question”
+     *  Given: History Helen presses the Start button
+     *  And: She states “Question. What caused the Roman Empire to fall”
+     *  When: Helen releases the Start button
+     *  Then: Her command of “Question” and her prompt “What caused the Roman Empire to fall?” are displayed in the Prompt Area with the command on-top of the prompt
+    */
+    @Test
+    public void BDD1() {
+        PromptFactory pf = new PromptFactory();
+        PromptPanel pp = new PromptPanel();
+        
+        // creates a question prompt and displays it in the prompt panel
+        IPrompt prompt1 = pf.createPrompt(PROMPT1);
+        pp.setPrompt(prompt1);
+
+        assert(prompt1 instanceof Question);                // prompt1 should have created a question
+
+        assertEquals(prompt1.toString(), pp.getPrompt());   // the display should match the prompt
+    }
+
+    /*  BDD Scenario Test for Scenario 2: Prompt starts with the word “Question,” but the question was not picked up
+     *  Given: History Helen presses and holds the start button
+     *  When: She states “Question.”
+     *  And: Helen Releases the Start button
+     *  Then: The command “Question” will be displayed with the message “Apologies, I am unable to fulfill your request for technical reasons on my part. Please try again.”
+     *  And: The prompt “Question” will be displayed in the history panel. 
+     */
+    @Test
+    public void BDD2() {
+        PromptFactory pf = new PromptFactory();
+
+        IPrompt prompt2 = pf.createPrompt(INVALID_QUESTION_COMMAND);
+
+        assert(prompt2 == null);    // inputting just the command should not create a prompt; the prompt should be null
+    }
+}
